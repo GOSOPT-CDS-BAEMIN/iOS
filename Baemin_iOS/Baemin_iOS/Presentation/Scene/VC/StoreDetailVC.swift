3a41971bbac40fc8a187be432d081910701e39b6
김응관 <kwhn9486@naver.com>
2023-05-19 16:38:30 +0900

    #9 [Add] 가게 대표 이미지 사진뷰 추가

--- Baemin_iOS/Baemin_iOS/Presentation/Scene/VC/StoreDetailVC.swift
+++ Baemin_iOS/Baemin_iOS/Presentation/Scene/VC/StoreDetailVC.swift
@@ -9,6 +9,16 @@ import UIKit
 import SnapKit
 
 class StoreDetailVC: UIViewController {
+    
+    // 1. 가게 대표 이미지 UIImageView
+    private let storeImage: UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage.dummy
+        img.contentMode = .scaleAspectFit
+        return img
+    }()
+    
+    // 2. 
 
     override func viewDidLoad() {
         super.viewDidLoad()
