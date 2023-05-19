6a0324ac4b58a868e0b96130de973827b2916833
김응관 <kwhn9486@naver.com>
2023-05-19 16:29:20 +0900

    #9 [Add] 뷰를 보기 위해 SceneDelegate의 scene 함수 작성

--- Baemin_iOS/Baemin_iOS/Application/SceneDelegate.swift
+++ Baemin_iOS/Baemin_iOS/Application/SceneDelegate.swift
@@ -11,9 +11,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            self.window = UIWindow(windowScene: windowScene)
+        
+            let navigationController = UINavigationController(rootViewController: StoreDetailVC())
+            self.window?.rootViewController = navigationController
+            self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ Baemin_iOS/Baemin_iOS/Presentation/Scene/VC/StoreDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  StoreDetailVC.swift
+//  Baemin_iOS
+//
+//  Created by 김응관 on 2023/05/19.
+//
+
+import UIKit
+import SnapKit
+
+class StoreDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
