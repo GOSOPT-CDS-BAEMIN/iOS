ab3e682a2810b98d1ad78fdba0afa509ed691670
hyj422 <hyj422@icloud.com>
2023-05-19 22:37:49 +0900

    #11 [Design] CartPriceView 중간 완성

--- Baemin_iOS/Baemin_iOS/Presentation/Scene/VC/CartViewController.swift
+++ Baemin_iOS/Baemin_iOS/Presentation/Scene/VC/CartViewController.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 
+import SnapKit
+
 class CartViewController: UIViewController {
+    private let cartView = CartPriceView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(cartView)
+        cartView.snp.makeConstraints {
+            $0.directionalHorizontalEdges.equalToSuperview().inset(50)
+            $0.top.bottom.equalToSuperview()
+        }
     }
     
 }
 
-//스크롤뷰, 컬렉션뷰, CartPriceView 불러오기
+// 스크롤뷰, 컬렉션뷰, CartPriceView 불러오기
--- Baemin_iOS/Baemin_iOS/Presentation/Scene/Views/CartPriceView.swift
+++ Baemin_iOS/Baemin_iOS/Presentation/Scene/Views/CartPriceView.swift
@@ -9,43 +9,78 @@ import UIKit
 import SnapKit
 
 class CartPriceView: UIView {
-
+    
+    // 1. 총 주문금액 StackView
+    
     private let totalPriceStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .fill
         stackView.alignment = .fill
+        stackView.backgroundColor = .white
         return stackView
     }()
     
-    private let totalPayStackView = UIStackView()
-        
+    private let emptyView_1 = UIView()
+    
     private let totalPriceLabel: UILabel = {
         let label = UILabel()
         label.text = I18N.Cart.totalPrice
+        label.font = .AppleSDGothicNeo(.regular, size: 16)
         return label
     }()
+    
+    var totalPriceValueLabel: UILabel = {
+        let label = UILabel()
+        label.font = .AppleSDGothicNeo(.regular, size: 16)
+        return label
+    }()
+
+    // 2. 결제 예상 금액 StackView
+    
+    private let totalPayStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.backgroundColor = .white
+        return stackView
+    }()
+    
+    private let emptyView_2 = UIView()
     
     private let totalPayLabel: UILabel = {
         let label = UILabel()
         label.text = I18N.Cart.totalPay
+        label.font = .AppleSDGothicNeo(.bold, size: 16)
         return label
     }()
     
-    private let noticeLabel: UILabel = {
+    var totalPayValueLabel: UILabel = {
         let label = UILabel()
-        label.text = I18N.Cart.notice
+        label.font = .AppleSDGothicNeo(.bold, size: 16)
         return label
     }()
-    
+        
     private let deliveryTipLabel: UILabel = {
         let label = UILabel()
         label.text = I18N.Cart.deliveryTip
+        label.font = .AppleSDGothicNeo(.regular, size: 10)
+        label.backgroundColor = .gray_2
+        label.textAlignment = .center
+        label.makeCornerRound(radius: 4)
         return label
     }()
     
-    var totalPriceValueLabel = UILabel()
-    var totalPayValueLabel = UILabel()
+    // 3. 주의사항 Label
+    
+    private let noticeLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = I18N.Cart.notice
+        label.font = .AppleSDGothicNeo(.regular, size: 12)
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -61,26 +96,61 @@ class CartPriceView: UIView {
 extension CartPriceView {
     
     func setStyle() {
-        addSubview(totalPriceStackView)
-        addSubview(totalPayStackView)
+        addSubviews(totalPriceStackView, totalPayStackView, noticeLabel)
+ 
     }
     
     func setLayout() {
-        totalPayStackView.addArrangedSubviews(totalPayLabel, totalPayValueLabel)
-        totalPriceStackView.addArrangedSubviews(totalPriceLabel, totalPriceValueLabel)
-        noticeLabel.addSubviews()
+        totalPriceStackView.addArrangedSubviews(totalPriceLabel,emptyView_1, totalPriceValueLabel)
+        totalPayStackView.addArrangedSubviews(totalPayLabel,deliveryTipLabel, emptyView_2,totalPayValueLabel)
         
-        totalPriceStackView.snp.makeConstraints{
-            $0.top.equalToSuperview()
+
+        totalPriceStackView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(71)
         }
-        totalPayStackView.snp.makeConstraints{
-            $0.top.equalTo(totalPriceStackView)
+        totalPriceLabel.snp.makeConstraints { // 이게 맞는 코드인지 모르겠어 stackview 이렇게 구성하는게 맞는건가
+            $0.leading.equalToSuperview()
+            $0.centerY.equalToSuperview()
         }
-        noticeLabel.snp.makeConstraints{
-            $0.top.equalTo(totalPayStackView)
+        totalPriceValueLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.centerY.equalToSuperview()
         }
+        emptyView_1.snp.makeConstraints {
+            $0.leading.equalTo(totalPriceLabel.snp.trailing)
+            $0.trailing.equalTo(totalPriceValueLabel.snp.leading)
+            $0.directionalVerticalEdges.equalToSuperview()
+        }
+        
+        totalPayStackView.snp.makeConstraints {
+            $0.top.equalTo(totalPriceStackView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(59)
+        }
+        totalPayLabel.snp.makeConstraints {
+            $0.leading.equalTo(totalPayStackView.snp.leading)
+            $0.centerY.equalTo(totalPayStackView.snp.centerY)
+        }
+        deliveryTipLabel.snp.makeConstraints {
+            $0.leading.equalTo(totalPayLabel.snp.trailing)
+            $0.centerY.equalTo(totalPayStackView.snp.centerY)
+        }
+        emptyView_2.snp.makeConstraints {
+            $0.leading.equalTo(deliveryTipLabel.snp.trailing)
+            $0.trailing.equalTo(totalPayValueLabel.snp.leading)
+            $0.directionalVerticalEdges.equalToSuperview()
+        }
+        totalPayValueLabel.snp.makeConstraints {
+            $0.trailing.equalTo(totalPayStackView)
+            $0.centerY.equalTo(totalPayStackView.snp.centerY)
+        }
+
+        noticeLabel.snp.makeConstraints {
+            $0.top.equalTo(totalPayStackView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(86)
+        }
+        
     }
-    
-    
-    
 }
