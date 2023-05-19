7356c5ae041e7d2b6c7d3d6a3730ba7d5c8ceec1
jeongdung-eo <jeuniis2@gmail.com>
2023-05-19 15:15:31 +0900

    #6 [Add] UILabel extension 추가

--- Baemin_iOS/Baemin_iOS/Global/Extensions/UILabel+.swift
+++ Baemin_iOS/Baemin_iOS/Global/Extensions/UILabel+.swift
@@ -7,6 +7,48 @@
 
 import UIKit
 
+extension UILabel {
+    /// 라벨 일부 textColor 변경해주는 함수
+    /// - targetString에는 바꾸고자 하는 특정 문자열을 넣어주세요
+    /// - textColor에는 targetString에 적용하고자 하는 특정 UIColor에 넣어주세요
+    func partColorChange(targetString: String, textColor: UIColor) {
+        let fullText = self.text ?? ""
+        let range = (fullText as NSString).range(of: targetString)
+        let attributedString = NSMutableAttributedString(string: fullText)
+        attributedString.addAttribute(.foregroundColor, value: textColor, range: range)
+        self.attributedText = attributedString
+    }
+    
+    /// 라벨 일부 font 변경해주는 함수
+    /// - targerString에는 바꾸고자 하는 특정 문자열을 넣어주세요
+    /// - font에는 targetString에 적용하고자 하는 UIFont를 넣어주세요
+    func partFontChange(targetString: String, font: UIFont) {
+        let fullText = self.text ?? ""
+        let range = (fullText as NSString).range(of: targetString)
+        let attributedString = NSMutableAttributedString(string: fullText)
+        
+        attributedString.addAttribute(.font, value: font, range: range)
+        self.attributedText = attributedString
+    }
+    
+    /// 라벨 일부 font와 color 변경해주는 함수
+    func setAttributedText(targetFontList: [String: UIFont],
+                           targetColorList: [String: UIColor]) {
+        let fullText = self.text ?? ""
+        let attributedString = NSMutableAttributedString(string: fullText)
+        for dic in targetFontList {
+            let range = (fullText as NSString).range(of: dic.key)
+            attributedString.addAttribute(.font, value: dic.value, range: range)
+        }
+        
+        for dic in targetColorList {
+            let range = (fullText as NSString).range(of: dic.key)
+            attributedString.addAttribute(.foregroundColor, value: dic.value, range: range)
+        }
+        self.attributedText = attributedString
+    }
+}
+
 extension NSMutableAttributedString {
     func addImageInBetweenString(firstSentence: String, image: UIImage?, lastSentence: String) -> NSMutableAttributedString {
         
