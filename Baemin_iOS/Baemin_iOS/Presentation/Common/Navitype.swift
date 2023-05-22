76abf6fff3b47923c33cae9f42982e7264615e8d
jeongdung-eo <jeuniis2@gmail.com>
2023-05-23 03:04:43 +0900

    #10 [Feat] 네비게이션 타입 분기처리

--- Baemin_iOS/Baemin_iOS/Presentation/Common/Navitype.swift
+++ Baemin_iOS/Baemin_iOS/Presentation/Common/Navitype.swift
@@ -8,18 +8,68 @@
 import UIKit
 
 enum NaviType {
-    case main, cart, menu
+    case main(SubMainItem), cart(SubCartItem), menu(SubMenuItem)
     
-    var rightIcon: UIImage {
-        switch self {
-        case .main, .menu: return UIImage.cart!
-        case .cart: return UIImage.people!
-        }
-    }
     var title: String {
         switch self {
         case .cart: return "장바구니"
         case .main, .menu: return ""
+        }
+    }
+}
+
+enum SubMainItem {
+    case leftButton, rightButton
+    
+    var leftIcon: UIImage {
+        switch self {
+        case .leftButton: return UIImage.arrow_back!
+        case .rightButton: return UIImage.home!
+        }
+    }
+    
+    var rightIcon: UIImage {
+        switch self {
+        case .leftButton: return UIImage.delivery!
+        case .rightButton: return UIImage.cart!
+        }
+    }
+}
+
+enum SubCartItem {
+    
+    case leftButton, rightButton
+    
+    var leftIcon: UIImage {
+        switch self {
+        case .leftButton: return UIImage.arrow_back!
+        case .rightButton: return UIImage.home!
+        }
+    }
+    
+    var rightIcon: UIImage? {
+        switch self {
+        case .rightButton: return UIImage.people!
+        case .leftButton: return nil
+        }
+    }
+}
+
+enum SubMenuItem {
+    
+    case leftButton, rightButton
+    
+    var leftIcon: UIImage {
+        switch self {
+        case .leftButton: return UIImage.arrow_back!
+        case .rightButton: return UIImage.home!
+        }
+    }
+    
+    var rightIcon: UIImage? {
+        switch self {
+        case .rightButton: return UIImage.cart!
+        case .leftButton: return nil
         }
     }
 }
