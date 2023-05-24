120df63fd0bee6fd28d29a09f6a3f93b3ed63426
jeongdung-eo <jeuniis2@gmail.com>
2023-05-24 19:35:01 +0900

    #20 [Feat] 컬렉션뷰 연결 및 기능 구현

--- Baemin_iOS/Baemin_iOS/Presentation/Scene/Main/VC/MainVC.swift
+++ Baemin_iOS/Baemin_iOS/Presentation/Scene/Main/VC/MainVC.swift
@@ -14,6 +14,7 @@ class MainVC: UIViewController {
     // MARK: - Properties
     
     var tabBarItems: [TabBarItem] = TabBarItem.tabBar()
+    var pageItems: [PagingItem] = PagingItem.titles
     private lazy var safeArea = self.view.safeAreaLayoutGuide
     
     // MARK: - UI Components
@@ -27,6 +28,7 @@ class MainVC: UIViewController {
     private lazy var tabBarcollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
+        layout.sectionInset = .init(top: 0, left: 18, bottom: 0, right: 18)
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .clear
@@ -38,11 +40,25 @@ class MainVC: UIViewController {
         return collectionView
     }()
     
+    private lazy var pageCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets.zero
+        layout.minimumLineSpacing = 0
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isPagingEnabled = true
+        
+        return collectionView
+    }()
+    
     // MARK: - Life Cycle
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.tabBarcollectionView.reloadData()
+        setupTabBarCollectioView()
     }
     
     override func viewDidLoad() {
@@ -50,15 +66,23 @@ class MainVC: UIViewController {
         setStyle()
         setLayout()
         register()
-
+        
     }
 }
 
 // MARK: - Methods
 
 extension MainVC {
+    
+    func setupTabBarCollectioView() {
+        tabBarcollectionView.isScrollEnabled = true
+        let indexPath = IndexPath(item: 0, section: 0)
+        tabBarcollectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
+    }
+    
     private func register() {
         tabBarcollectionView.register(cell: MainTabbarCell.self)
+        pageCollectionView.register(cell: MainPageCell.self)
     }
     
     private func setStyle() {
@@ -66,33 +90,86 @@ extension MainVC {
     }
     
     private func setLayout() {
-        view.addSubview(tabBarcollectionView)
+        view.addSubviews(tabBarcollectionView, pageCollectionView)
         
         tabBarcollectionView.snp.makeConstraints {
             $0.top.equalTo(safeArea).offset(52)
             $0.directionalHorizontalEdges.equalTo(safeArea)
             $0.height.equalTo(44)
         }
+        
+        pageCollectionView.snp.makeConstraints {
+            $0.top.equalTo(tabBarcollectionView.snp.bottom).offset(20)
+            $0.directionalHorizontalEdges.bottom.equalToSuperview()
+        }
     }
 }
 
 // MARK: - Delegate, DataSource
 
-extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return tabBarItems.count
     }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: MainTabbarCell = collectionView.dequeueReusableCell(for: indexPath)
-        let item = tabBarItems[indexPath.row]
-        cell.dataBind(item: item)
-        return cell
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        let targetXContentOffset = targetContentOffset.pointee.x
+        let itemAt = Int(targetXContentOffset / self.view.frame.width)
+        let indexPath = IndexPath(item: itemAt, section: 0)
+        
+        tabBarcollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        tabBarcollectionView.selectItem(at: indexPath, animated: true, scrollPosition: [])
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == tabBarcollectionView {
+            let cell: MainTabbarCell = collectionView.dequeueReusableCell(for: indexPath)
+            let item = tabBarItems[indexPath.item]
+            cell.bind(item)
+            return cell
+        } else if collectionView == pageCollectionView {
+            let cell: MainPageCell = collectionView.dequeueReusableCell(for: indexPath)
+            let item = pageItems[indexPath.item]
+            cell.bind(item)
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedCellIndex = indexPath.item
+        if collectionView == tabBarcollectionView {
+            let middleIndex = collectionView.bounds.width / 2
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            let desiredOffsetX = CGFloat(selectedCellIndex) * collectionView.bounds.width - middleIndex
+            
+            let pageIndexPath = IndexPath(item: selectedCellIndex, section: 0)
+            pageCollectionView.scrollToItem(at: pageIndexPath, at: .centeredHorizontally, animated: true)
+            
+        } else if collectionView == pageCollectionView {
+            let middleIndex = collectionView.bounds.width / 2
+            let desiredOffsetX = CGFloat(selectedCellIndex) * collectionView.bounds.width - middleIndex
+            let targetOffset = CGPoint(x: desiredOffsetX, y: 0)
+            collectionView.setContentOffset(targetOffset, animated: true)
+        }
+    }
+}
+
+// MARK: - CollectionViewDelegateFlowLayout
+
+extension MainVC: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-         let cell: MainTabbarCell = collectionView.dequeueReusableCell(for: indexPath)
-               cell.tabBarLabel.text = tabBarItems[indexPath.row].name
-               cell.tabBarLabel.sizeToFit()
-               let cellWidth = cell.tabBarLabel.frame.width
-               return CGSize(width: cellWidth, height: 40)
-            }
+        if collectionView == tabBarcollectionView {
+            let cell: MainTabbarCell = collectionView.dequeueReusableCell(for: indexPath)
+            cell.tabBarLabel.text = tabBarItems[indexPath.row].name
+            cell.tabBarLabel.sizeToFit()
+            let cellWidth = cell.tabBarLabel.frame.width + 20
+            return CGSize(width: cellWidth, height: 40)
+        } else if collectionView == pageCollectionView {
+            return CGSize(width: pageCollectionView.frame.width, height: pageCollectionView.frame.height)
+        }
+        return CGSize.zero
+    }
 }
