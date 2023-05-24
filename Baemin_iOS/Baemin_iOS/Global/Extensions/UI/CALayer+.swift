15df2f803ea990c2c2f2813134a221e65fe2b084
jeongdung-eo <jeuniis2@gmail.com>
2023-05-25 08:49:41 +0900

    #33 [Add] CALayer extension 추가

---
+++ Baemin_iOS/Baemin_iOS/Global/Extensions/UI/CALayer+.swift
@@ -0,0 +1,24 @@
+//
+//  CALayer+.swift
+//  Baemin_iOS
+//
+//  Created by JEONGEUN KIM on 2023/05/25.
+//
+
+
+import UIKit
+
+extension CALayer {
+    func applyShadow(
+        color: UIColor? = .black,
+        alpha: Float = 1,
+        x: CGFloat = 2,
+        y: CGFloat = 4,
+        blur: CGFloat = 6
+    ) {
+        shadowColor = color?.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+    }
+}
