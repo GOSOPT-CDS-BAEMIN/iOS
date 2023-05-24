07d4501085904d23a8f59efdd89a923b4a3afd8e
김응관 <kwhn9486@naver.com>
2023-05-25 00:33:23 +0900

    #22 [Merge] 로컬 feat/#22와 원격 feat/#22 브랜치 병합

--- Baemin_iOS/Baemin_iOS/Network/Base/Base.swift
+++ Baemin_iOS/Baemin_iOS/Network/Base/Base.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 enum Config {
+<<<<<<< HEAD
 
     enum Keys {
         enum Plist {
@@ -15,6 +16,16 @@ enum Config {
         }
     }
 
+=======
+    
+    enum Keys {
+        enum Plist {
+            static let baseURL = "BASE_URL"
+            static let api = "MOVIE_API"
+        }
+    }
+    
+>>>>>>> 8a1112a (#21 [Add] API 연결 파일, 폴더 추가)
     private static let infoDictionary: [String: Any] = {
         guard let dict = Bundle.main.infoDictionary else {
             fatalError("plist cannot found.")
@@ -24,11 +35,25 @@ enum Config {
 }
 
 extension Config {
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 8a1112a (#21 [Add] API 연결 파일, 폴더 추가)
     static let baseURL: String = {
         guard let key = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
             fatalError("Base URL is not set in plist for this configuration.")
         }
         return key
     }()
+<<<<<<< HEAD
+=======
+    
+    static let api: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.api] as? String else {
+            fatalError("Base URL is not set in plist for this configuration.")
+        }
+        return key
+    }()
+>>>>>>> 8a1112a (#21 [Add] API 연결 파일, 폴더 추가)
 }
