2249d53ec2df71a2ab76687faad8e81550317520
김응관 <kwhn9486@naver.com>
2023-05-26 00:44:38 +0900

    #22 [Merge] develop과 충돌해결

--- Baemin_iOS/Baemin_iOS/Presentation/Scene/Views/Section3/StickyHeaderView.swift
+++ Baemin_iOS/Baemin_iOS/Presentation/Scene/Views/Section3/StickyHeaderView.swift
@@ -11,5 +11,80 @@ import SnapKit
 
 class StickyHeaderView: UITableViewHeaderFooterView {
     
+    private let menuLabel: UILabel = {
+        let label = UILabe()
+        label.text = I18N.StickyHeader.menu
+        label.font = .AppleSDGothicNeo(.semiBold, size: 16)
+        label.sizeToFit()
+        return label
+    }()
+    
+    private let infoLabel: UILable = {
+        let label = UILabe()
+        label.text = I18N.StickyHeader.info
+        label.font = .AppleSDGothicNeo(.regular, size: 16)
+        label.textColor = .gray_5
+        label.sizeToFit()
+        return label
+    }()
+    
+    private let speaker: UIImageView = {
+        let icon = UIImageView()
+        icon.image = .speaker.resized(toWidth: 18)
+        icon.sizeToFit()
+        icon.contentMode = .scaleAspectFit
+        
+        return icon
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.backgroundColor = .clear
+        stackView.sizeToFit()
+        return stackView
+    }()
+    
+    private let review: UILable = {
+        let label = UILabe()
+        label.text = I18N.StickyHeader.review
+        label.font = .AppleSDGothicNeo(.regular, size: 16)
+        label.textColor = .gray_5
+        label.sizeToFit()
+        return label
+    }()
+    
+    private let segmentControl: UISegmentedControl = {
+        let control = SegmentControl(items: ["메뉴", "정보", "리뷰"])
+        control.translatesAutoresizingMaskIntoConstraints = false
+        control.sizeToFit()
+        return control
+    }
+    
+    // init
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setStyle()
+        setInfoStack()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setInfoStack() {
+        stackView.addArrangedSubviews(infoLabel, speaker)
+    }
+    
+    func setStyle() {
+        contentView.backgroundColor = .clear
+    }
+    
+    func setSegment() {
+        self.segmentControl.setTitleAttributes(
+            [NS]
+        )
+    }
     
 }
