dfc0541009ce0714561473d88c19e5a4aab88b3e
김응관 <kwhn9486@naver.com>
2023-05-26 02:35:31 +0900

    #39 [Add] 메뉴, 정보, 리뷰 탭 마다의 ViewController 파일 추가

---
+++ Baemin_iOS/Baemin_iOS/Presentation/Scene/Views/Section3/VC/InfoVC.swift
@@ -0,0 +1,29 @@
+//
+//  InfoVC.swift
+//  Baemin_iOS
+//
+//  Created by 김응관 on 2023/05/26.
+//
+
+import UIKit
+
+class InfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
