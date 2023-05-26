67e0a385826abad5bf458477f45560bbeb3aa2ee
jeongdung-eo <jeuniis2@gmail.com>
2023-05-27 02:46:40 +0900

    #46 [Fix] 이미지 url 변경

--- Baemin_iOS/Baemin_iOS/Presentation/Scene/Main/Cell/ImageCacher.swift
+++ Baemin_iOS/Baemin_iOS/Presentation/Scene/Main/Cell/ImageCacher.swift
@@ -48,7 +48,7 @@ class ImageCacheManager {
 
 extension UIImageView {
   func getImageFromURL(_ url: String) {
-      let imageURL = Config.baseURL + "/\(url)"
+      let imageURL =  url
       print(imageURL)
     ImageCacheManager.getImage(imageURL) { result in
       switch result {
