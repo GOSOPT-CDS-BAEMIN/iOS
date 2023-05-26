3b246760e4e8cdc66820871606d8a6d42ad23972
jeongdung-eo <jeuniis2@gmail.com>
2023-05-26 21:07:17 +0900

    #43 [Feat] StoreAPI 세팅

--- Baemin_iOS/Baemin_iOS/Network/API/Store/StoreAPI.swift
+++ Baemin_iOS/Baemin_iOS/Network/API/Store/StoreAPI.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+import Alamofire
+
+class StoreAPI: BaseAPI {
+    static let shared = MainAPI()
+    
+    private(set) var getMainStoreData: StoreResponseDTO?
+    private override init() {}
+}
+
+extension StoreAPI {
+    public func getStoreInfo(request: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+           AFManager.request(StoreService.getStore(id: request)).responseData { response in
+               print("✅: \(response)")
+               self.disposeNetwork(response,
+                                   dataModel: StoreResponseDTO.self,
+                                   completion: completion)
+           }
+       }
+}
--- Baemin_iOS/Baemin_iOS/Network/DTO/Store/StoreResponseDTO.swift
+++ Baemin_iOS/Baemin_iOS/Network/DTO/Store/StoreResponseDTO.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+// MARK: - DailyMissionResponseDTO
+struct StoreResponseDTO: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: StoreDataClass
+}
+
+// MARK: - DataClass
+struct StoreDataClass: Codable {
+    let storeInfo: StoreInfo
+    let foods: [Food]
+}
+
+// MARK: - Food
+struct Food: Codable {
+    let foodID: Int
+    let foodName: String
+    let price: Int
+    let foodDescription, foodImageURL: String
+    let best: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case foodID = "foodId"
+        case foodName, price, foodDescription
+        case foodImageURL = "foodImageUrl"
+        case best
+    }
+}
+
+// MARK: - StoreInfo
+struct StoreInfo: Codable {
+    let storeID, storeTypeID: Int
+    let storeType, storeName, deliveryTime, description: String
+    let minOrderPrice, deliveryFee: Int
+    let rate: Double
+    let firstImageURL, secondImageURL, thirdImageURL: String
+    let new, couponExist: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case storeID = "storeId"
+        case storeTypeID = "storeTypeId"
+        case storeType, storeName, deliveryTime, description, minOrderPrice, deliveryFee, rate
+        case firstImageURL = "firstImageUrl"
+        case secondImageURL = "secondImageUrl"
+        case thirdImageURL = "thirdImageUrl"
+        case new, couponExist
+    }
+}
+
+extension StoreInfo {
+    static var item: [StoreInfo] = [StoreInfo(storeID: 1, storeTypeID: 1, storeType: "1인분", storeName: "스토어1", deliveryTime: "10분~30분", description: "Store 1 description", minOrderPrice: 10000, deliveryFee: 2000, rate: 4.5, firstImageURL: "url1", secondImageURL: "url2", thirdImageURL: "url3", new: false, couponExist: true)]
+}
